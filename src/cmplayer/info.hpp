20d606b88479a802b922ded89e0980e0db4c9e6c
xylosper <darklin20@gmail.com>
2013-03-02 11:51:31 +0900

    update to 0.7.1

--- src/cmplayer/info.hpp
+++ src/cmplayer/info.hpp
@@ -23,7 +23,7 @@ public:
 	static QStringList audioNameFilter() {return self.m_audioExt.toNameFilter();}
 	static QStringList subtitleNameFilter() {return self.m_subExt.toNameFilter();}
 	static QStringList playlistNameFilter() {return self.m_plExt.toNameFilter();}
-	static const char *version() {return "0.7.0";}
+	static const char *version() {return "0.7.1";}
 	static const char *name() {return "CMPlayer";}
 	static QString mediaExtFilter();
 	static QStringList mediaNameFilter() {return videoNameFilter() += audioNameFilter();}
