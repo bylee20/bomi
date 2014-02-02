6733c46eb02baaa4957fb7954070d7d14831b34a
xylosper <darklin20@gmail.com>
2014-02-02 15:51:13 +0900

    * Fix: 'Apply in fullscreen mode only' for 'Hide mouse cursor' was always checked

--- src/cmplayer/prefdialog.cpp
+++ src/cmplayer/prefdialog.cpp
@@ -510,7 +510,7 @@ void PrefDialog::set(const Pref &p) {
 	d->ui.enable_generate_playlist->setChecked(p.enable_generate_playist);
 	d->ui.generate_playlist->setCurrentValue(p.generate_playlist);
 	d->ui.hide_cursor->setChecked(p.hide_cursor);
-	d->ui.hide_cursor_fs_only->setChecked(p.hide_cursor);
+	d->ui.hide_cursor_fs_only->setChecked(p.hide_cursor_fs_only);
 	d->ui.hide_delay->setValue(p.hide_cursor_delay/1000);
 	d->ui.disable_screensaver->setChecked(p.disable_screensaver);
 	d->ui.remember_image->setChecked(p.remember_image);
