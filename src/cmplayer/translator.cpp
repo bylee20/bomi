a9684bea405612489d9c359d8e8e598b52c80fa6
xylosper <darklin20@gmail.com>
2014-02-06 22:55:41 +0900

    * Fix: convert ISO 639-2 B code to localized language name

--- src/cmplayer/translator.cpp
+++ src/cmplayer/translator.cpp
@@ -1,6 +1,8 @@
 #include "translator.hpp"
 #include <unicode/locid.h>
 
+struct Iso639_2 { QString b, t; };
+
 struct Translator::Data {
 	bool succ = false;
 	QTranslator trans;
@@ -8,6 +10,7 @@ struct Translator::Data {
 	QSet<QLocale> locales;
 	icu::Locale icu;
 	QMap<QString, QString> langs;
+	QHash<QString, QString> b2t;
 };
 
 static inline uint qHash(const QLocale &key) {
@@ -34,6 +37,32 @@ Translator::Translator()
 	d->path = QString::fromLocal8Bit(qgetenv("CMPLAYER_TRANSLATION_PATH"));
 	if (!d->path.isEmpty())
 		d->locales += getLocales(d->path, "*.qm", "(.*).qm");
+
+	d->b2t[_L("cze")] = _L("ces");
+	d->b2t[_L("baq")] = _L("eus");
+	d->b2t[_L("fre")] = _L("fra");
+	d->b2t[_L("ger")] = _L("deu");
+	d->b2t[_L("gre")] = _L("ell");
+	d->b2t[_L("arm")] = _L("hye");
+	d->b2t[_L("ice")] = _L("isl");
+	d->b2t[_L("geo")] = _L("kat");
+	d->b2t[_L("mac")] = _L("mkd");
+	d->b2t[_L("mao")] = _L("mri");
+	d->b2t[_L("may")] = _L("msa");
+	d->b2t[_L("bur")] = _L("mya");
+	d->b2t[_L("dut")] = _L("nld");
+	d->b2t[_L("per")] = _L("fas");
+	d->b2t[_L("rum")] = _L("ron");
+	d->b2t[_L("slo")] = _L("slk");
+	d->b2t[_L("alb")] = _L("sqi");
+	d->b2t[_L("tib")] = _L("bod");
+	d->b2t[_L("wel")] = _L("cym");
+	d->b2t[_L("chi")] = _L("zho");
+
+	// custom code for opensubtitles
+	d->b2t[_L("scc")] = _L("srp");
+	d->b2t[_L("pob")] = _L("por");
+	d->b2t[_L("pb")] = _L("pt");
 }
 
 Translator::~Translator() {
@@ -72,14 +101,20 @@ QString Translator::defaultEncoding() {
 	return tr("UTF-8", "Specify most popular encoding here in target localization.");
 }
 
-QString Translator::displayLanguage(const QString &iso) {
+QString Translator::displayLanguage(const QString &_iso) {
 	auto d = get().d;
+	QString iso = _iso.toLower();
+	auto it = d->b2t.constFind(iso);
+	if (it != d->b2t.cend())
+		iso = *it;
 	auto &name = d->langs[iso];
 	if (name.isEmpty()) {
 		icu::UnicodeString str;
 		icu::Locale locale(iso.toLatin1());
 		locale.getDisplayLanguage(d->icu, str);
 		name.setUtf16(str.getBuffer(), str.length());
+		if (iso == name)
+			qDebug() << "Cannot find locale for" << iso;
 	}
 	return name;
 }
