18d70a217800a9e80327f3ad82f556a9c6e3133c
xylosper <darklin20@gmail.com>
2015-02-10 19:39:04 +0900

    bump up version to 0.9.1

--- src/bomi/player/app.hpp
+++ src/bomi/player/app.hpp
@@ -43,7 +43,7 @@ public:
     template<class T>
     auto sendMessage(MessageType type, const T &t, int timeout = 5000)
     -> tmp::enable_if_t<tmp::is_one_of<T, QJsonObject, QJsonArray, QJsonValue>(), bool>;
-    static constexpr auto version() -> const char* { return "0.9.0"; }
+    static constexpr auto version() -> const char* { return "0.9.1"; }
     static constexpr auto name() -> const char* { return "bomi"; }
     static auto displayName() -> QString { return tr("bomi"); }
     static auto defaultIcon() -> QIcon;
