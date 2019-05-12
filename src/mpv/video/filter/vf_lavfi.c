443f6713706486149db313582e70afe993d23468
Taegil Bae <esrevinu@gmail.com>
2019-05-12 23:14:29 +0900

    Fix error in building mpv solely

--- src/mpv/video/filter/vf_lavfi.c
+++ src/mpv/video/filter/vf_lavfi.c
@@ -34,7 +34,6 @@
 #include <libavutil/error.h>
 #include <libswscale/swscale.h>
 #include <libavfilter/avfilter.h>
-#include <libavfilter/avfiltergraph.h>
 #include <libavfilter/buffersink.h>
 #include <libavfilter/buffersrc.h>
 
